5a8475e9bf2860463695aad42eb2a3f8ed018fe7
Chi Chang <edingroot@gmail.com>
2018-03-07 01:14:32 +0800

    Alter table structures.

--- src/tw.ntust.dripmonitor/logger/setup/db/seed.sql
+++ src/tw.ntust.dripmonitor/logger/setup/db/seed.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Mar 03, 2018 at 07:05 PM
+-- Generation Time: Mar 06, 2018 at 05:13 PM
 -- Server version: 5.5.56-MariaDB
 -- PHP Version: 5.6.32
 
@@ -24,7 +24,7 @@ CREATE TABLE `event_log` (
   `sn` int(11) NOT NULL,
   `event_code` tinyint(4) NOT NULL DEFAULT '0',
   `message` text COLLATE utf8_unicode_ci,
-  `mac_adapter` varchar(16) COLLATE utf8_unicode_ci NOT NULL,
+  `mac_adapter` varchar(16) COLLATE utf8_unicode_ci DEFAULT NULL,
   `mac_drip` varchar(16) COLLATE utf8_unicode_ci DEFAULT NULL,
   `src_ip` varchar(39) COLLATE utf8_unicode_ci DEFAULT NULL,
   `src_port` int(11) DEFAULT NULL,
@@ -53,7 +53,9 @@ CREATE TABLE `tcp_log_stream` (
 -- Indexes for table `event_log`
 --
 ALTER TABLE `event_log`
-  ADD PRIMARY KEY (`sn`);
+  ADD PRIMARY KEY (`sn`),
+  ADD KEY `event_code` (`event_code`),
+  ADD KEY `created_at` (`created_at`);
 
 --
 -- Indexes for table `tcp_log_stream`
@@ -69,9 +71,9 @@ ALTER TABLE `tcp_log_stream`
 -- AUTO_INCREMENT for table `event_log`
 --
 ALTER TABLE `event_log`
-  MODIFY `sn` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `sn` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34656;
 --
 -- AUTO_INCREMENT for table `tcp_log_stream`
 --
 ALTER TABLE `tcp_log_stream`
-  MODIFY `sn` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7806;
+  MODIFY `sn` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11149;
