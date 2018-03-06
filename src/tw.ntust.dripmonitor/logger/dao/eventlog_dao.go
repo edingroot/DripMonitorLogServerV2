b75a31ccbba8a2df130b0eebf15d8d0b459c86b7
Chi Chang <edingroot@gmail.com>
2018-03-06 21:32:57 +0800

    Update event log dao.

--- src/tw.ntust.dripmonitor/logger/dao/eventlog_dao.go
+++ src/tw.ntust.dripmonitor/logger/dao/eventlog_dao.go
@@ -6,8 +6,6 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
-const LogTagELD = "[EventLogDAO]"
-
 type EventLogDAO struct {
 	db *sql.DB
 }
@@ -18,24 +16,26 @@ func NewEventLogDAO(db *sql.DB) *EventLogDAO {
 	}
 }
 
-func (dao *EventLogDAO) InsertRecord(record *datamodels.EventRecord) bool {
-	var err error
-	var stmtIns *sql.Stmt
+func (d *EventLogDAO) InsertRecord(record *datamodels.EventRecord) bool {
+	errorResult := false
 
 	query := "insert into event_log (event_code, message, mac_adapter, mac_drip, src_ip, src_port) values (?,?,?,?,?,?)"
-	stmtIns, err = dao.db.Prepare(query)
+	stmtIns, err := d.db.Prepare(query)
 	if err != nil {
-		log.Errorf("%s Error preparing insert: %s", LogTagELD, err.Error())
-		return false
+		d.logError(err); return errorResult
 	}
 	defer stmtIns.Close()
 
 	rs := record.SQLForm()
 	_, err = stmtIns.Exec(rs.EventCode, rs.Message, rs.AdapterMAC, rs.DripMAC, rs.SrcIP, rs.SrcPort)
 	if err != nil {
-		log.Errorf("%s Error inserting record: %s", LogTagELD, err.Error())
-		return false
+		d.logError(err); return errorResult
 	}
 
 	return true
+}
+
+
+func (d *EventLogDAO) logError(err error) {
+	log.Errorf("[EventLogDAO] Error occurred: " + err.Error())
 }
